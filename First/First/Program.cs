14b764baf83f535adddb7172d8eff0c783b30f24
김태민 <103544589+kimtaemin27@users.noreply.github.com>
2024-09-16 13:02:23 +0900

    09-16 first

---
+++ First/First/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace First
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int result;
+            result = 100 - 50;
+            Console.WriteLine(result);
+        }
+    }
+}
