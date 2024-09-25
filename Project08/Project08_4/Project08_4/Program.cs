5c9bb390ae448f28e88cc5ffb85987e1b6404669
김태민 <103544589+kimtaemin27@users.noreply.github.com>
2024-09-25 12:29:28 +0900

    24-09-25/2

---
+++ Project08/Project08_4/Project08_4/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Project08_4
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            // 두 문자열을 비교하는 CompareTo() 메서드
+            // 문자열의 포함을 확인하는 Contains() 메서드
+            string str1 = "C# Programming";
+            string str2 = "C# IT CookBook";
+
+            Console.WriteLine("원 문자열1 => [" + str1 + "]");
+            Console.WriteLine("원 문자열2 => [" + str2 + "]");
+
+            Console.WriteLine(str1.CompareTo(str2));
+            Console.WriteLine(str1.Contains("C#"));
+
+
+            // 문자열이 숫자인지 확인하는 Char.IsDigit() 메서드
+            string str;
+            bool numYN = true;
+
+            Console.Write("문자열 입력 => ");
+            str = Console.ReadLine();
+
+            Console.Write(str + " --> ");
+            foreach(char c in str)
+            {
+                if(!Char.IsDigit(c))
+                    numYN = false;
+            }
+
+            if (!numYN)
+                Console.WriteLine("숫자가 아닙니다");
+            else
+                Console.WriteLine("숫자입니다");
+        }
+
+    }
+
+}
---
+++ Project08/Project08_5/Project08_5/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Project08_5
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            // 입력한 문자의 개수세기 및 거꾸로 출력하기
+            string str;
+            int lowerCnt = 0, upperCnt = 0, numCnt = 0, hanCnt = 0, etcCnt = 0;
+
+            Console.Write("문자열 입력 => ");
+            str = Console.ReadLine();
+
+            foreach(char c in str)
+            {
+                if ('a' <= c && c <= 'z')
+                    lowerCnt++;
+                else if ('A' <= c && c <= 'Z')
+                    upperCnt++;
+                else if ('0' <= c && c <= '9')
+                    numCnt++;
+                else if ('가' <= c && c <= '힣')
+                    hanCnt++;
+                else
+                    etcCnt++;
+            }
+
+            Console.WriteLine("소문자 : {0}, 대문자 : {1}, 숫자 : {2}, 한글 : {3}, 기타 : {4}", lowerCnt, upperCnt, numCnt, hanCnt, etcCnt);
+
+            Console.WriteLine();
+            Console.Write("거꾸로 출력 => ");
+            for(int i = str.Length - 1; i >= 0; i--)
+                Console.Write(str[i]);
+            Console.WriteLine();
+        }
+
+    }
+
+}
